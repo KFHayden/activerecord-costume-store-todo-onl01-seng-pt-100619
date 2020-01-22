c781a08ade2caca169050c93b9ef3dd565d4e258
Kevin Hayden <kevin.fhayden@gmail.com>
2020-01-22 23:26:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,13 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      :location
+      :theme
+      :price
+      :
+      :
+      :
+      :
 end
