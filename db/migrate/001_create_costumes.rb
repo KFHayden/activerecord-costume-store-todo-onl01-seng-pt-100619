64e972160c084ecfea5e2c18565c43c8e98f3e95
Kevin Hayden <kevin.fhayden@gmail.com>
2020-01-22 23:31:16 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,6 +14,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.text :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
