d9afaba587b1bd6cef8c2e5dc246cda55e4cae2e
Kevin Hayden <kevin.fhayden@gmail.com>
2020-01-22 23:23:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :integer
+      t.string :size
       t.text :image_url
     end
   end
